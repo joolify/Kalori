3b3b37cb5a94e878c866e036c9dd7ec440489df4
Joel <joelwe@gmail.com>
2018-09-25 22:31:59 +0200

    new csv file

--- Inkopslista/Migrations/Configuration.cs
+++ Inkopslista/Migrations/Configuration.cs
@@ -19,6 +19,19 @@ namespace Inkopslista.Migrations
         }
          protected override void Seed(Inkopslista.Models.ApplicationDbContext context)
         {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = "LivsmedelsDB_20180925.csv"; 
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                Console.WriteLine(stream);
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    CsvReader csvReader = new CsvReader(reader);
+                    csvReader.Configuration.MissingFieldFound = null;
+                    var foods = csvReader.GetRecords<Food>().ToArray();
+                    context.Foods.AddOrUpdate(c => c.Id, foods);
+                }
+            }
         }
     }
 }
